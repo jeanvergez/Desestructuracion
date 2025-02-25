ba88281d675584ab26e96f01822f83c58cfc681f
Jean Evins <130567240+jeanvergez@users.noreply.github.com>
2025-02-25 13:48:50 -0400

    Add files via upload

---
+++ Desestructuracionyspread.js
@@ -0,0 +1,12 @@
+const jugador = {
+    nombre: "Steve",
+    experiencia: 15,
+    inventario: {
+        herramienta: "Pico de diamante",
+        comida: "Manzana dorada",
+        bloques: 64
+
+    }
+};
+
+const nuevoObjeto = "Antorcha";
